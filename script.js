f3697820789b7e20b7ded5b8e66f359b363da8a4
akashgamage <bakashgamage@gmail.com>
2022-01-17 23:39:52 +0530

    Update script.js
    left right buttons add & view card no. changed

--- script.js
+++ script.js
@@ -4,19 +4,25 @@ $(document).ready(function () {
         loop: true,
         margin: 10,
         responsiveClass: true,
+        center: true,
+        nav: true,
+        navText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
         responsive: {
             0: {
                 items: 1,
-                nav: true
+                /*nav: true*/
             },
             600: {
                 items: 3,
-                nav: false
+                /*nav: false*/
             },
             1000: {
-                items: 5,
-                nav: true,
-                loop: false
+                items: 3,
+                /*nav: true,
+                loop: false*/
             }
         }
     })
